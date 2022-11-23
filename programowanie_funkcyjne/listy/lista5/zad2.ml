09ffe041b403c81c3668f3138e13f608543572cc
Łukasz Magnuszewski <lukasz.magnuszewski@gmail.com>
2022-11-23 22:56:46 +0100

    pf.l5.z2 połowa

--- programowanie_funkcyjne/listy/lista5/zad2.ml
+++ programowanie_funkcyjne/listy/lista5/zad2.ml
@@ -6,11 +6,16 @@ let rec oldfix f x = f (oldfix f) x;;
 
 
 let mutfix (f : (('a -> 'b) -> 'a -> 'b)) (x : 'a) =
-  let placeholder (f : (('a -> 'b) -> 'a -> 'b)) (x : 'a) : 'b = failwith "abc" in
+  let placeholder (f : (('a -> 'b) -> 'a -> 'b)) (x : 'a) : 'b = assert false in
   let fix =  ref placeholder in
   fix := (fun (f : (('a -> 'b) -> 'a -> 'b)) (x : 'a) -> f (!fix f) x);
   f (!fix f) x
+
+let zmutfix =
+  let placeholder (f : (('a -> 'b) -> 'a -> 'b)) (x : 'a) : 'b = assert false in
+  let fix =  ref placeholder in
+  fix := (fun (f : (('a -> 'b) -> 'a -> 'b)) (x : 'a) -> f (!fix f) x);
+  !fix
  
 
-type 'a fixik =
-| Fix of 'a * (unit -> 'a fixik)
+
