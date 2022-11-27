a3063bde6a868c01de88a7bfdac7d20557099754
Łukasz Magnuszewski <lukasz.magnuszewski@gmail.com>
2022-11-27 17:58:43 +0100

    imperatywne guwno do 4 bo nie umiem zwracać paray

--- programowanie_funkcyjne/listy/lista5/zad4.ml
+++ programowanie_funkcyjne/listy/lista5/zad4.ml
@@ -1,6 +1,6 @@
 type 'a dllist = 'a dllist_data Lazy.t
 (* Note: lazy_t is the built-in type constructor used by the compiler for the lazy keyword. You should not use it directly. Always use Lazy.t instead. *)
-and 'a dllist_data ={ prev : 'a dllist; elem : 'a; next : 'a dllist }
+and 'a dllist_data ={ elem : 'a; next : 'a dllist; prev : 'a dllist;  }
 
 let prev (prev : 'a dllist) = 
   match prev with
@@ -16,4 +16,30 @@ let elem (prev : 'a dllist) =
 
 
 
+let sing x =
+  let rec s = lazy {next = s; elem = x; prev = s} in
+  s
 
+let rec gen (prev : 'a dllist) xs first =
+  match xs with
+  | [] -> first, prev
+  | x :: xs -> 
+    let last = ref (sing x) in
+    let rec z = lazy begin
+        let (a,b) = gen z xs first in
+       last := b; {prev = prev; elem = x; next = a} 
+    end in
+    let k = (Lazy.force z) in
+    lazy k, !last 
+
+
+
+
+let rec cycle xs =
+  match xs with
+  | [] -> failwith "puuusto"
+  | x :: xs -> let last = ref (sing x) in
+    let rec first = lazy begin
+      let (a,b) = gen first xs first in
+      last := b; {prev = b; elem = x; next = a}
+    end in first
