7faf7a21d58fc62eed7243f0fdf56ec6d0517a12
Łukasz Magnuszewski <lukasz.magnuszewski@gmail.com>
2022-11-30 14:47:22 +0100

    lista 6 mdm

--- programowanie_funkcyjne/listy/lista5/zad2.ml
+++ programowanie_funkcyjne/listy/lista5/zad2.ml
@@ -10,19 +10,28 @@ let mutfix (f : (('a -> 'b) -> 'a -> 'b)) (x : 'a) =
 
 
 
+let lambda_calc_fix f = (fun x -> f (x x)) (fun x -> f (x x)) ;;
+
+
 
 type 'a lambda_fix = Fix of ('a lambda_fix -> 'a)
-
-
-  let unfix = function
-  | Fix f -> f
-
-let y t x = let p (Fix f) = t (f (Fix f)) in 
+let unfix = function
+| Fix f -> f
+let fixop f = (fun x a -> f (unfix x x)a ) (Fix (fun x a -> f (unfix x x)a)) ;;
+let fix t x = let p (Fix f) = t (f (Fix f)) in 
   p (Fix p) x
 
-let fix f = (fun x a -> f (unfix x x) a) (Fix (fun x a -> f (unfix x x) a));;
 
-let notfix f = (fun x -> f (x x)) (fun x -> f (x x));;
-let fix f = (fun (`A x) -> f(x (`A x))) (`A(fun (`A x) y -> f(x (`A x)) y));;
+let fix f a = (fun (`A x) -> f(x (`A x))) (`A(fun (`A x) y -> f(x (`A x)) y)) a;;
+
+
+
+
+
+
+
+
+
+
 
 
