a55ee39bfeb50b7289956eaffce59306dd645f16
Łukasz Magnuszewski <lukasz.magnuszewski@gmail.com>
2022-12-09 17:14:03 +0100

    haskielllik pierwwsze podejścia

---
+++ programowanie_funkcyjne/listy/lista8/z2.hs
@@ -0,0 +1,36 @@
+import Data.Char(toUpper)
+import System.IO( isEOF )
+
+
+data StreamTrans i o a = 
+      Return a 
+    | ReadS (Maybe i -> StreamTrans i o a)
+    | WriteS o (StreamTrans i o a)
+
+toLower :: StreamTrans Char Char a
+toLower = ReadS (\ x ->
+    case x of
+        Nothing -> toLower 
+        Just x -> WriteS (toUpper x) (toLower ))
+
+runIOStreamTrans :: StreamTrans Char Char a -> IO a
+runIOStreamTrans (Return a) = do
+    return a
+runIOStreamTrans (ReadS f) = do
+    end <- isEOF
+    if end then 
+        runIOStreamTrans (f Nothing)
+    else do
+        x <- getChar
+        runIOStreamTrans $ f (Just x)
+runIOStreamTrans (WriteS o s) = do
+    putChar o
+    runIOStreamTrans s 
+
+helper :: StreamTrans i o a -> [i] -> [o] -> ([o], a)
+helper (Return a) _ o = (o, a)
+helper (ReadS f) [] o = helper (f Nothing) [] o
+helper (ReadS f) (x : xs) o = helper (f (Just x)) xs o 
+helper (WriteS o s) xs o = helper s xs (s : o)
+listTrans :: StreamTrans i o a -> [i] -> ([o], a)
+listTrans (Return a) _ = ([], a)
