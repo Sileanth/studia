00c9dc4e3f343f6b925bb832669b2a4651677c34
sileanth <lukasz.magnuszewski@gmail.com>
2023-12-09 14:37:55 +0100

    nix usntable

--- typki/listy/l3/flake.nix
+++ typki/listy/l3/flake.nix
@@ -3,7 +3,7 @@
 
   # Flake inputs
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2305.491812.tar.gz";
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
   };
 
   # Flake outputs
