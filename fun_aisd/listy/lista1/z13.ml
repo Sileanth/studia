b2ee68888d7268ad3fd500b67c72e6a8efd09327
Sileanth <lukasz.magnuszewski@gmail.com>
2024-02-27 13:30:53 +0100

    z13 fun aids

--- fun_aisd/listy/lista1/z13.ml
+++ fun_aisd/listy/lista1/z13.ml
@@ -5,7 +5,7 @@ type tree = Leaf | Node of int * tree * tree
 
 let rec member x = function
   | Leaf -> false
-  | Tree(y, tl, tr) ->
+  | Node(y, tl, tr) ->
       if x <= y then
         if y <= x then true
         else member x tl
@@ -25,22 +25,37 @@ let rec mem2 x t =
   | Leaf -> false 
   | Node(y, _, _) ->
       let rec help c = function 
-        | Leaf -> c = t 
+        | Leaf -> c = x 
         | Node(y, a, b) ->
             if x < y then help c a
             else help y b
       in help y t 
 
-Exception: Same
-let rec ins2 x t
+
+exception Same
+let rec ins2 x t =
   let rec helper c = function 
     | Leaf -> if c = x 
             then raise Same 
             else Node(x, Leaf, Leaf)
     | Node(y, a, b) -> 
-
-
-  try helper x t
+        if x < y 
+        then Node(y, helper c a, b)
+        else Node(y, a, helper y b)
+  in try helper x t
   with Same -> t
 
-
+let ins3 x t =
+  match t with
+  | Leaf -> Node(x, Leaf, Leaf)
+  | Node(y, _, _) ->
+      let rec helper c t = 
+        match t with
+        | Leaf when c = x -> raise Same
+        | Leaf -> Node(x, Leaf, Leaf)
+        | Node(y, a, b) ->
+            if x < y 
+            then Node(y, helper c a, b)
+            else Node(y, a, helper y b)
+      in try helper y t 
+      with Same -> t 
